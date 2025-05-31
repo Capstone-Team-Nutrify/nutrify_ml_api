977178d1735abcde56d85c11ef67e040df6b2299
AnamSadat <anamsadat3@gmail.com>
2025-05-31 22:38:49 +0700

    fix error response

--- app/routes.py
+++ app/routes.py
@@ -16,20 +16,29 @@ def index():
      
 @main.route("/predict", methods = ["POST"])
 def predict_route():
-    data = request.get_json()
-    print(data)
-    
-    if not data or "bahan" not in data:
-        return jsonify({"error": "one or more of the bahan no provided"}), 400
-    
     try:
-        respone = handle_prediction(data)
+        data = request.get_json()
+        print("Received data:", data)
+
+        if not data or "makanan" not in data:
+            return jsonify({"error": "Field 'makanan' is missing"}), 400
+
+        makanan_input = data["makanan"]
+
+        # Optional: cek validitas masing-masing item
+        for item in makanan_input:
+            if "bahan" not in item or "dose" not in item:
+                return jsonify({"error": "Each item must have 'bahan' and 'dose'"}), 400
+
+        response = handle_prediction(data)
+
         result = {
             "status": 200,
             "message": "success",
-            "description": "success insert to mongodb database",
-            "predict": respone
+            "predict": response
         }
         return jsonify(result), 200
+
     except Exception as e:
+        print("Error:", e)
         return jsonify({"error": str(e)}), 500
